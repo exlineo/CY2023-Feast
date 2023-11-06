0685aa8a52f985092c3d56d126f8b29b1f481e2b
FabriqueNumerique <fabriquenumeriquepaloise@gmail.com>
2023-11-06 11:29:16 +0100

    initialisation de l'application CY 2023 Feast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-racine',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'CY2023-feast';
+  soustitre:string = "L'application de gestion de vos soirées";
+  moustache:string = 'Affiche moi des moustache steuplé {{ !';
+  listeMoustaches:Array<string> = ['Moustache', 'Favoris'];
+  autreListeMoustaches:string[] = [];
+
+  constructor(){};
+
+  peupleMoustaches(){
+    console.log('Peuple moustache appelé');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { MenuComponent } from './template/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
